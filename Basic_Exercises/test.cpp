d7d714d7d3f381def72aa910d5e92b45dc342a87
Mark Delchev <markdelchev@gmail.com>
2022-09-01 13:26:42 +0300

    test.cpp

--- Basic_Exercises/test.cpp
+++ Basic_Exercises/test.cpp
@@ -1,9 +1,8 @@
 #include <iostream>
-
 using namespace std;
 int main()
-{   
-    cout << "Hello World" << endl;
+{
+    string hello = "Hello World!";
+    cout << hello << endl;
     return 0;
 }
-
